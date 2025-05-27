9ecdaa3ed339f615b18650cfae788f2a6cc76408
kryllyxofficial01 <kryllyxofficial@gmail.com>
2025-05-27 17:10:22 -0400

    simplify config path registry key reading

--- src/config_loader.c
+++ src/config_loader.c
@@ -1,41 +1,29 @@
 #include "include/config_loader.h"
 
 result_t* open_config() {
+    char config_path[BENJI_BASIC_STRING_LENGTH];
+    unsigned long config_path_length = sizeof(config_path);
+
     #if defined(_WIN32)
-        HKEY hkey;
-
-        char config_path[BENJI_BASIC_STRING_LENGTH];
-        unsigned long config_path_length = sizeof(config_path);
-
-        HRESULT hresult = RegOpenKeyExA(
+        HRESULT hresult = RegGetValueA(
             HKEY_LOCAL_MACHINE,
             BENJI_CONFIG_REGISTRY_PATH,
-            0, KEY_READ, &hkey
+            BENJI_CONFIG_REGISTRY_KEY,
+            RRF_RT_REG_SZ | RRF_NOEXPAND,
+            NULL,
+            (void*) config_path,
+            &config_path_length
         );
 
         if (FAILED(hresult)) {
-            return result_error(hresult, BENJI_ERROR_PACKET, "RegOpenKeyEx() failed");
-        }
-
-        unsigned long type = REG_SZ;
-
-        hresult = RegQueryValueExA(
-            hkey, BENJI_CONFIG_REGISTRY_KEY, NULL, &type, (unsigned char*) config_path, &config_path_length
-        );
-
-        if (FAILED(hresult)) {
-            return result_error(hresult, BENJI_ERROR_PACKET, "RegQueryValueEx() failed");
-        }
-
-        hresult = RegCloseKey(hkey);
-
-        if (FAILED(hresult)) {
-            return result_error(hresult, BENJI_ERROR_PACKET, "RegCloseKey() failed");
+            return result_error(hresult, BENJI_ERROR_PACKET, "RegGetValueA() failed");
         }
 
         config_path[config_path_length] = '\0';
 
         log_message(BENJI_LOG_LEVEL_INFO, "Collected config path from Windows registry: '%s'", config_path);
+    #elif defined(__linux__)
+        /* TODO: add linux stuff */
     #endif
 
     FILE* file = fopen(config_path, "r");
